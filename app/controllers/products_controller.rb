4d57c040053e3b0f6899a5832e4dbf976e816b73
maigen skjelstad & Patti Jurgensen <student@epicodus.com>
2014-04-11 12:37:41 -0700

    add destroy function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,6 +33,12 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, notice: "Product has been deleted"
+  end
+
 private
 
   def product_params
