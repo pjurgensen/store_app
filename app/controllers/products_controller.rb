6c9ec618699f7ce2cdaf7742c3cf855c6a9e7f63
maigen skjelstad & Patti Jurgensen <student@epicodus.com>
2014-04-11 11:57:09 -0700

    add new and create functions with respective view pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,25 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "Product Saved"
+    else
+      render 'new', notice: "Invalid Entry. Please try again."
+    end
+  end
+
+private
+
+  def product_params
+    params.require(:product).permit(:name,
+                                    :description,
+                                    :price)
+  end
 end
