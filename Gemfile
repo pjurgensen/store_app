49bcaf5b018f2f742b0a57ac6fe89aa080dde640
maigen skjelstad & Patti Jurgensen <student@epicodus.com>
2014-04-11 09:59:58 -0700

    add tables and models for products, and initial set up

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+gem 'pg'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jquery-turbolinks'
+gem "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
+gem 'bootstrap-sass'
+gem 'bcrypt'
+gem 'masonry-rails'
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'quiet_assets'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'factory_girl'
+  gem 'pry'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoreApp::Application.config.secret_key_base = '199fc21d1429d9a102980d4d820d9468155c6d5e94d6f246cd61855ce570e4975b5d13ea8874abe1526d35f729aedb845840405ce90ef30c0133c18dbe0eb1f9'
